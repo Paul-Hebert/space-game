cf54e2aac0731c19fe53e7633b49e64d57d9322f
Paul Hebert <paul@cloudfour.com>
2022-10-10 09:21:46 -0700

    Delete obsolete filres

--- scripts/objects/bullets/pew.js
+++
@@ -1,9 +0,0 @@
-import { BaseBullet } from "./base.js";
-
-export class PewBullet extends BaseBullet {
-  age = 0;
-  maxAge = 20;
-  radius = 4;
-  fill = "red";
-  damage = 5;
-}
--- scripts/objects/bullets/ray.js
+++
@@ -1,9 +0,0 @@
-import { BaseBullet } from "./base.js";
-
-export class RayBullet extends BaseBullet {
-  age = 0;
-  maxAge = 40;
-  radius = 10;
-  fill = "blue";
-  damage = 10;
-}
