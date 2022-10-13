f9e74c76a1a61cf953812cd848b7df1402f2d4ff
Paul Hebert <paul@cloudfour.com>
2022-10-13 12:54:04 -0700

    Gun changes

--- scripts/ships/base.js
+++ scripts/ships/base.js
@@ -1,8 +1,8 @@
 import { Pew } from "../weapons/pew.js";
 import { Ray } from "../weapons/ray.js";
 import { Laser } from "../weapons/laser.js";
-import { GodMode } from "../weapons/god-mode.js";
 import { BaseWeapon } from "../weapons/base-weapon.js";
+import { randomInt } from "../math/random.js";
 
 export class BaseShip {
   constructor({ x = 0, y = 0, speed = { x: 0, y: 0 }, rotation = 0 }) {
@@ -10,6 +10,8 @@ export class BaseShip {
     this.y = y;
     this.speed = speed;
     this.rotation = rotation;
+
+    this.currentGun = randomInt(0, this.weapons.length - 1);
   }
 
   health = 100;
@@ -21,15 +23,9 @@ export class BaseShip {
 
   accelerationSpeed = 0.1;
 
-  weapons = [
-    new BaseWeapon(),
-    new Pew(),
-    new Ray(),
-    new Laser(),
-    new GodMode(),
-  ];
-
-  currentGun = 0;
+  weapons = [new BaseWeapon(), new Pew(), new Ray(), new Laser()];
 
   maxSpeed = 15;
+
+  currentGun = 0;
 }
--- scripts/ships/player.js
+++ scripts/ships/player.js
@@ -1,7 +1,26 @@
 import { BaseShip } from "./base.js";
+import { Pew } from "../weapons/pew.js";
+import { Ray } from "../weapons/ray.js";
+import { Laser } from "../weapons/laser.js";
+import { GodMode } from "../weapons/god-mode.js";
+import { BaseWeapon } from "../weapons/base-weapon.js";
 
 export class PlayerShip extends BaseShip {
+  constructor(params) {
+    super(params);
+
+    this.currentGun = 0;
+  }
+
   resourceCount = 0;
   maxHealth = 1000;
   health = 1000;
+
+  weapons = [
+    new Pew(),
+    new BaseWeapon(),
+    new Ray(),
+    new Laser(),
+    new GodMode(),
+  ];
 }
