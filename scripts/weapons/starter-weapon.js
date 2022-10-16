6494b32071e8a6ebcd08cf4c08cba9b04a41021e
Paul Hebert <paul@cloudfour.com>
2022-10-15 21:33:12 -0700

    Weapon tweaks for starter difficulty

--- scripts/weapons/starter-weapon.js
+++ scripts/weapons/starter-weapon.js
@@ -2,12 +2,12 @@ import { BaseWeapon } from "./base-weapon.js";
 
 export class StarterWeapon extends BaseWeapon {
   name = "pew";
-  speed = 30;
+  speed = 25;
   reloadSpeed = 6;
   bulletRadius = 3;
   bulletColor = "red";
-  damage = 5;
-  maxAge = 50;
+  damage = 3;
+  maxAge = 30;
 
   shoot(playerState) {
     return this.bulletStream(playerState, 20, 2);
