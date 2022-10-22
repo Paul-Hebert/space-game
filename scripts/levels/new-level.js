f72fce5c2d935100e340f0fc325f4eea6a5a2849
Paul Hebert <paul@cloudfour.com>
2022-10-22 12:09:43 -0700

    Restore health in between levels

--- scripts/levels/new-level.js
+++ scripts/levels/new-level.js
@@ -1,6 +1,10 @@
+import { updateHealthBar } from "../hud/update-health-bar.js";
 import { resetMap } from "../state/map-data.js";
+import { playerState } from "../state/player-state.js";
 
 export function newLevel(level) {
+  playerState.health = playerState.maxHealth;
+  updateHealthBar();
   resetMap();
 
   level();
