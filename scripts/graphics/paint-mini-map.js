8297dfba35d18ffb03b296e2cb7bc446adb696b4
Paul Hebert <paul@cloudfour.com>
2022-11-06 10:11:21 -0800

    Display upgrades on minimap

--- scripts/graphics/paint-mini-map.js
+++ scripts/graphics/paint-mini-map.js
@@ -3,6 +3,7 @@ import { miniMapCtx, clearMiniMap, miniMapCanvas } from "./canvas.js";
 import { playerState } from "../state/player-state.js";
 import { rotatedDraw } from "./rotated-draw.js";
 import { mapSize } from "../map-size.js";
+import { drawCircle } from "./draw-circle.js";
 
 const enemyShipGraphic = document.getElementById("minimap-ship-enemy");
 const playerShipGraphic = document.getElementById("minimap-ship-player");
@@ -19,6 +20,16 @@ export function paintMiniMap() {
       graphic: enemyShipGraphic,
     });
   });
+
+  mapData.resources
+    .filter((resource) => resource.type === "weapon-upgrade")
+    .forEach((resource) => {
+      drawCircle(miniMapCtx, {
+        ...relativeMiniMapPosition(resource),
+        radius: 5,
+        fill: resource.fill,
+      });
+    });
 
   drawMiniMapShip({
     ...playerState,
