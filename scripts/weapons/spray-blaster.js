7a160fb06b5fed9e89d5ea51ac722a8c9e1f67a6
Paul Hebert <paul@cloudfour.com>
2022-11-20 12:12:01 -0800

    Changes to spray blaster

--- scripts/weapons/spray-blaster.js
+++ scripts/weapons/spray-blaster.js
@@ -4,12 +4,12 @@ export class SprayBlaster extends SprayGun {
   name = "Mach III Dispersal Agent";
   description = "Not super powerful but it sprays in a wide radius.";
 
-  sprayPoints = [-30, -15, 0, 15, 30];
+  sprayPoints = [-20, -10, 0, 10, 20];
 
   reloadSpeed = 10;
   bulletRadius = 8;
   damage = 12;
-  bulletsPerShot = 1;
+  bulletsPerShot = 2;
   bulletSpeed = 70;
   bulletColor = "green";
 }
